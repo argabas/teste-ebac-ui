90f8de2514d882d4c7f9eae4f744b62d4e43afab
argabas <argabas@outlook.com>
2022-04-03 17:19:18 -0300

    Endereco

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import enderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereço - Faturamento e Entrega', () => {
     
@@ -11,7 +13,21 @@ describe('Funcionalidade Endereço - Faturamento e Entrega', () => {
     });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
-        //cadastro de endereço
+        enderecoPage.editarEnderecoFaturamento('Jo', 'Sa', 'Da', 'Brasil', 'Rua', '01', 'Jir', 'São Paulo', '13579-246', '01 23456-7890', 'josa@teste.com')
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso usando arquivo de dados', () => {    
+        enderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email)
     });
 });
